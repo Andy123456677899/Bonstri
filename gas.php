21cc6325dd273cc1f8af321b0546eb627216e9ef
Andy123456677899 <76278290+Andy123456677899@users.noreply.github.com>
2020-12-22 18:01:39 +0700

    Update gas.php

--- gas.php
+++ gas.php
@@ -8,7 +8,7 @@
 include 'tri_req.php';
 
 $tri = new tri();
-$imei = "868880043302499";
+$imei = "868812035219368";
 echo "Masukkan No Telepon : ";
 $msisdn = trim(fgets(STDIN));
 $otp = $tri->request_otp($msisdn,$imei);
